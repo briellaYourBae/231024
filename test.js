e4c89d8014ce4953266aa726b79c53f38530236b
briellaYourBae <briellayb@gmail.com>
2023-10-24 12:40:00 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,16 @@
+//latiham pertama
+console.log("Hello Word");
+console.log("XI " + "PPLG" + " 1");
+console.log("BriellaYB");
+
+//angka
+console.log(99 % 5);
+console.log(99 + 1 - 1);
+
+//TypeData
+let nama = "Abdul Aziz Taslim";
+console.log(nama);
+
+//karakteristil
+let angka = 99;
+console.log(angka + 1);
